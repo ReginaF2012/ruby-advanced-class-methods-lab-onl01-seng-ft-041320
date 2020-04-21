3b8d009169e5b2dfd631253f6a4a2ecfaa145c4e
Regina Furness <skritschy@gmail.com>
2020-04-21 00:47:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,7 @@ class Song
     song_attributes = filename.split(".")
     song_attributes.pop
     song = self.new
-    song.name = song_attributes[0]
+    song.name = song_attributes[1]
     song.artist_name = song_attributes[-1]
     song
   end
