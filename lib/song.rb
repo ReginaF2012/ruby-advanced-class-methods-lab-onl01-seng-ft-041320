adb7ffbc6d72eec509d1230a22278a6ea2c1a222
Regina Furness <skritschy@gmail.com>
2020-04-21 00:42:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,7 @@ class Song
   def self.find_or_create_by_name(song_name)
     @@all.find do |song|
       if song == self.find_by_name(song_name)
+        self.find_by_name(song_name)
       else
         self.create_by_name(song_name)
       end 
