2405a002b569f5fadedc8903bfd9784890f95e13
Regina Furness <skritschy@gmail.com>
2020-04-21 00:37:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,24 +11,31 @@ class Song
     self.class.all << self
   end
 
-  def self.create #class constructor
+  def self.create
     song = self.new
     self.all << song
     song
   end
 
-  def self.new_by_name(title) #class constructor
+  def self.new_by_name(song_name)
     song = self.new
-    song.name = title
+    song.name = song_name
     song
   end
 
-  def self.create_by_name(title) #class constructor
+  def self.create_by_name(song_name)
     song = self.create
-    song.name = title
+    song.name = song_name
     song
   end
-
+  
+  def self.find_by_name(song_name)
+    @all.each do |song|
+      if song.name == song.song_name
+        song.song_name
+    end 
+  end 
+    
   def self.new_from_filename(filename)
     song_attributes = filename.split("-")
     song_attributes = fiename.split(".")
