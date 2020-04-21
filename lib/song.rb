41fb655684d274e40b8d3e39647888d85d38f930
Regina Furness <skritschy@gmail.com>
2020-04-21 01:05:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
   def self.new_from_filename(filename)
     song_attributes = filename.split(" ")
     song = self.new
-    song.name = "#{song_attributes[0]} #{song_attributes[1]}"
+    song.name = "#{song_attributes[2]} #{song_attributes[3]} #{song_attributes[4]}"
     song.artist_name = song_attributes[0]
     song
   end
